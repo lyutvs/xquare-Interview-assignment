474aac6a739ea08c1d13e023ce555969207bba62
lyutvs <lyutvs@gmail.com>
2022-05-30 23:13:01 +0900

    📑 ::  User

---
+++ user-infrastructure/src/main/kotlin/user/entity/User.kt
@@ -0,0 +1,37 @@
+package user.entity
+
+import com.example.xquareinterviewassignment.configuration.entity.BaseUUIDEntity
+import org.hibernate.validator.constraints.Length
+import user.type.Authority
+import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+import javax.persistence.Table
+import javax.validation.constraints.NotNull
+
+@Entity
+@Table(name = "tbl_user")
+class User(
+
+    @field:NotNull
+    @field:Length(max = 50)
+    val accountId: String,
+
+    @field:NotNull
+    @field:Length(max = 60)
+    val password: String,
+
+    @field:NotNull
+    @field:Length(max = 16)
+    val name: String,
+
+    authority: Authority
+
+) : BaseUUIDEntity() {
+
+    @field:NotNull
+    @field:Enumerated(EnumType.STRING)
+    var authority: Authority = authority
+        protected set
+
+}
